5afc7db4a1b8adeb6e0fbf537d4df8c1ade07254
Robert Barnes <robertbarnes1981@googlemail.com>
2015-02-19 07:51:32 +0000

    use link function for some links

--- templates/item/starred.php
+++ templates/item/starred.php
@@ -1,6 +1,6 @@
 <h2>Starred</h2>
 <div class="well">
-    <a href="/home/view" class="btn btn-info">Channels</a>
+	<?php echo Html::link('Channels', Url::get('home', 'view'), array('class' => 'btn btn-info')); ?>
     <?php echo $pagination; ?>
 </div>
 <div class="list-group">
@@ -11,14 +11,15 @@
         <?php } else { ?>
             <span class="glyphicon"></span>
         <?php } ?>
-        <a href="/item/view?id=<?php echo $item->id; ?>" class="-btn -btn-default -btn-xs">
+		<?php echo Html::link($item->title, Url::get('item', 'view', array('id' => $item->id)), array('class' => '-btn -btn-default -btn-xs')); ?>
             <?php echo $item->title;?>
         </a>
         <span class="label label-default"><?php echo $item->pubdate; ?></span>
-            <?php if ($item->unread) { ?>
+		<?php if ($item->unread) { ?>
         <a class="btn btn-danger btn-xs pull-right" href="/item/state?id=<?php echo $item->id; ?>&unread=false&return=<?php echo urlencode('/item/starred'); ?>">Mark Read</a>
+		<?php echo Html::link('Mark Read', Url::get('item', 'state', array('id' => $item->id, 'unread' => 'false', 'return' => urlencode(Url::get('item', 'starred')))), array('class' => 'btn btn-danger btn-xs pull-right')); ?>
         <?php } else { ?>
-        <a class="btn btn-success btn-xs pull-right" href="/item/state?id=<?php echo $item->id; ?>&unread=true&return=<?php echo urlencode('/item/starred'); ?>">Mark Unread</a>
+		<?php echo Html::link('Mark Unread', Url::get('item', 'state', array('id' => $item->id, 'unread' => 'true', 'return' => urlencode(Url::get('item', 'starred')))), array('class' => 'btn btn-success btn-xs pull-right')); ?>
         <?php } ?>
     </div>
 <?php } ?>
